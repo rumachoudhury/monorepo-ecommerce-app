291110b176390aa2828b7f2beed771a85ce5d39d
Ruma Choudhury <rumachoudhury7862@gmail.com>
2026-05-13 01:36:48 -0400

    Completed some parts of the UsersPage

--- apps/admin/src/app/users/page.tsx
+++ apps/admin/src/app/users/page.tsx
@@ -248,6 +248,15 @@ function UsersPage() {
 
           <button> + Add User</button>
         </div>
+
+        {/* User Stats */}
+        <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-6 mb-10">
+          <div className="bg-white rounded-xl shadow p-6 transition hover:shadow-xl hover:translate-1">
+            <p className="text-gray-500">Total Users</p>
+            <h2 className="text-3xl font-bold mt-2">1,248</h2>
+            <p className="">+12% this month</p>
+          </div>
+        </div>
       </main>
     </div>
   );
