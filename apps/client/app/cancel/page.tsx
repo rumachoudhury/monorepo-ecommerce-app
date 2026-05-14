a40d7ce878000364320a0718a53ad60389ab152a
Ruma Choudhury <rumachoudhury7862@gmail.com>
2026-05-14 00:55:50 -0400

    Created some file

--- apps/client/app/cancel/page.tsx
+++ apps/client/app/cancel/page.tsx
@@ -1,9 +1,7 @@
-// export default function CancelPage() {
-//   return (
-//     <div className="p-10">
-//       <h1 className="text-2xl font-bold text-red-500">
-//         Payment Cancelled
-//       </h1>
-//     </div>
-//   );
-// }
+export default function CancelPage() {
+  return (
+    <div className="p-10">
+      <h1 className="text-2xl font-bold text-red-500">Payment Cancelled</h1>
+    </div>
+  );
+}
--- apps/client/app/success/page.tsx
+++ apps/client/app/success/page.tsx
@@ -1,10 +1,8 @@
-// "use client";
-// export default function SuccessPage() {
-//   return (
-//     <div className="p-10">
-//       <h1 className="text-2xl font-bold text-green-600">
-//         Payment Successful 🎉
-//       </h1>
-//     </div>
-//   );
-// }
+"use client";
+export default function SuccessPage() {
+  return (
+    <div className="p-8">
+      <h1 className="text-2xl font-bold text-green-600">Payment Successful</h1>
+    </div>
+  );
+}
