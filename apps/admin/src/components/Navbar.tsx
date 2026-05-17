7a0b82dc750ddbb6702e8b4b6b46e8da44061744
Ruma Choudhury <rumachoudhury7862@gmail.com>
2026-05-17 00:30:59 -0400

    Working with navbar

--- apps/admin/src/components/Navbar.tsx
+++ apps/admin/src/components/Navbar.tsx
@@ -1,35 +1,64 @@
-// "use client";
+"use client";
 
-// import { SearchIcon } from "lucide-react";
-// import React, { useState } from "react";
+import { SearchIcon } from "lucide-react";
+import React, { useState } from "react";
 
-// export default function Navbar() {
-//   const [search, setSearch] = useState("");
+export default function Navbar() {
+  const [search, setSearch] = useState("");
 
-//   const handleSearch = () => {
-//     console.log(handleSearch);
-//     setSearch(""); //clear input
-//   };
+  const handleSearch = () => {
+    console.log(handleSearch);
+    setSearch(""); //clear input
+  };
+  return (
+    <div className="w-full h-20 bg-gray-900 text-white flex items-center justify-center">
+      <div className="flex items-center w-96 bg-gray-800 border border-gray-600 rounded-lg px-3 py-2">
+        <SearchIcon
+          onClick={handleSearch}
+          className="text-gray-400 w-4 h-4 mr-2"
+        />
+
+        <input
+          onChange={(e) => setSearch(e.target.value)}
+          onKeyDown={(e) => {
+            if (e.key === "Enter") {
+              handleSearch();
+            }
+          }}
+          type="text"
+          placeholder="Search..."
+          className="bg-transparent outline-none w-full text-white"
+        />
+      </div>
+    </div>
+  );
+}
+
+// import { Image } from "lucide-react";
+// import React from "react";
+
+// function Navbar() {
 //   return (
-//     <div className="w-full h-20 bg-gray-900 text-white flex items-center justify-center">
-//       <div className="flex items-center w-96 bg-gray-800 border border-gray-600 rounded-lg px-3 py-2">
-//         <SearchIcon
-//           onClick={handleSearch}
-//           className="text-gray-400 w-4 h-4 mr-2"
-//         />
-
-//         <input
-//           onChange={(e) => setSearch(e.target.value)}
-//           onKeyDown={(e) => {
-//             if (e.key === "Enter") {
-//               handleSearch();
-//             }
-//           }}
-//           type="text"
-//           placeholder="Search..."
-//           className="bg-transparent outline-none w-full text-white"
+//     <div>
+//       {/* LOGO */}
+//       <div className="flex items-center gap-2">
+//         <Image
+//           src="/woliul-hasan-BfPkm4ptlsI-unsplash.jpg"
+//           alt="Logo"
+//           width={40}
+//           height={40}
+//           className="rounded-full object-cover"
 //         />
 //       </div>
+//       <input
+//         type="text"
+//         placeholder="Search..."
+//         className="border border-gray-500 text-white bg-gray-900"
+//       />
+
+//       <div></div>
 //     </div>
 //   );
 // }
+
+// export default Navbar;
