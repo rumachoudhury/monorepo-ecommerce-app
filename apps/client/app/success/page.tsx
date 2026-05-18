c6f02fc61ff557aa3f8f17a3a81efc7af79594a7
Ruma Choudhury <rumachoudhury7862@gmail.com>
2026-05-18 11:42:55 -0400

    Working with SuccessPage

--- apps/client/app/success/page.tsx
+++ apps/client/app/success/page.tsx
@@ -1,8 +1,20 @@
 "use client";
+
+import { CheckCircle } from "lucide-react";
+import Link from "next/link";
+
 export default function SuccessPage() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-50 px-4">
-      <h1 className="text-2xl font-bold text-green-600">Payment Successful</h1>
+      <div className="max-w-md w-full bg-white rounded-2xl shadow-lg p-8 text-center">
+        {/* Icon */}
+        <div>
+          <CheckCircle className="w-16 h-16" />
+        </div>
+        <h1></h1>
+        <p></p>
+        <Link href="/"></Link>
+      </div>
     </div>
   );
 }
