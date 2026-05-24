9bf9d0a166d4027890ee3fbe5a595ddb344ba7e2
Ruma Choudhury <rumachoudhury7862@gmail.com>
2026-05-24 00:34:49 -0400

    Fixed some part

--- apps/client/app/products/page.tsx
+++ apps/client/app/products/page.tsx
@@ -6,7 +6,7 @@ export default function ProductsPage() {
       <h1 className="text-3xl font-bold">Products</h1>
 
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 mt-6">
-        <ProductCard id="1" title="Nike Shoes" price={150} image="/shoe.avif" />
+        <ProductCard id="1" title="Nike Shoes" price={120} image="/shoe.avif" />
         <ProductCard
           id="2"
           title="Apple Watch"
@@ -19,7 +19,7 @@ export default function ProductsPage() {
           price={89}
           image="/headphone.avif"
         />
-        <ProductCard id="4" title="Parfum" price={89} image="/parfum.avif" />
+        <ProductCard id="4" title="Parfum" price={90} image="/parfum.avif" />
         <ProductCard
           id="5"
           title="Sunglasses"
